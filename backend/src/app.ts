66cc788be067070d364bbf2a79d818534365cf8d
Ali Ghasemi <ali.gha3mi75@gmail.com>
2026-07-03 16:38:28 +0330

    increse upload size

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,8 +17,8 @@ const app = express()
 
 app.use(helmet())
 app.use(cors({ origin: config.cors.origin, credentials: true }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 if (config.nodeEnv === 'development') {
   app.use(morgan('dev'))
